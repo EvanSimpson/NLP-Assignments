77a3fc505adfb47d1a8eec055a3e09cc7e1d5d02
Evan Simpson <esimpson.rescue@gmail.com>
2013-05-06 01:38:21 -0400

    Adding final version of assignment 3.

--- Assignment3.py
+++ Assignment3.py
@@ -7,12 +7,13 @@ Assignment 3
 
 from collections import defaultdict
 import json
+import codecs
 
 class FileIterator:
 
 	def __init__ (self, enFile, esFile):
-		self.en = open(enFile, 'r')
-		self.es = open(esFile, 'r')
+		self.en = codecs.open(enFile, 'r', encoding='utf-8')
+		self.es = codecs.open(esFile, 'r', encoding='utf-8')
 
 	def __iter__(self):
 		return self
@@ -31,16 +32,25 @@ class FileIterator:
 		self.en.seek(0)
 		self.es.seek(0)
 
+
+###############################################################################
+###############################################################################
+###############################################################################
+
+
 class Translator:
 
-	def __init__(self, teFile, tfFile, eFile, fFile, outFile, paramFile=""):
+	def __init__(self, teFile, tfFile, eFile, fFile, outFile, model, paramFile=""):
 		self.eFile = eFile
 		self.fFile = fFile
 		self.outFile = outFile
 		self.paramFile = paramFile
 		self.alignments = []
 		self.parameters = self.importParams()
-		self.algorithm = IBM1(FileIterator(teFile, tfFile), FileIterator(eFile, fFile), 5, self.parameters)
+		if model == 1:
+			self.algorithm = IBM1(FileIterator(teFile, tfFile), FileIterator(eFile, fFile), 5, self.parameters)
+		else:
+			self.algorithm = IBM2(FileIterator(teFile, tfFile), FileIterator(eFile, fFile), 5, self.parameters)
 		
 	def importParams(self):
 		print self.paramFile
@@ -65,6 +75,12 @@ class Translator:
 			for alignment in self.alignments:
 				z.write("%d %d %d" %(alignment[0],alignment[1],alignment[2])+'\n')
 
+
+###############################################################################
+###############################################################################
+###############################################################################
+
+
 class keydefaultdict(defaultdict):
     def __missing__(self, key):
         if self.default_factory is None:
@@ -72,6 +88,12 @@ class keydefaultdict(defaultdict):
         else:
             ret = self[key] = self.default_factory(key)
             return ret
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+
 
 class IBM1:
 
@@ -101,7 +123,7 @@ class IBM1:
 	def sigma(self, i, j, eSentence, fSentence):
 		num = self.parameters[(eSentence[j], fSentence[i])]
 		denom = 0.0
-		for l in [0] + range(j, len(eSentence)):
+		for l in range(len(eSentence)):
 			denom += self.parameters[(eSentence[l], fSentence[i])]
 		return num/denom
 
@@ -126,7 +148,7 @@ class IBM1:
 
 	def exportParameters(self):
 		print "Writing parameters to file..."
-		with open("parameters2.txt", 'w') as z:
+		with open("parameters.txt", 'w') as z:
 			for param in self.parameters.items():
 				z.write(json.dumps(param) + '\n')
 		print "Done writing parameters to file."
@@ -150,12 +172,105 @@ class IBM1:
 		self.alignments.sort()
 		return self.alignments
 
+###############################################################################
+###############################################################################
+###############################################################################
+
+class IBM2:
+
+	def __init__(self, tIterator, oIterator, iters, parameters):
+		self.trainIterator = tIterator
+		self.transIterator = oIterator
+		self.iterations = iters
+		self.ewords = defaultdict(set)
+		self.export = False
+		self.q = keydefaultdict(lambda q: 1.0/(q[2]+1))
+		if parameters != {}:
+			print "Imported parameters."
+			self.parameters = parameters
+		else:
+			self.export = True
+			print "Initializing parameters..."
+			self.parameters = self.initializeParameters()
+			print "Done initializing parameters."
+		self.alignments = []
+
+	def initializeParameters(self):
+		parameters = keydefaultdict(lambda e: 1.0/len(self.ewords[e[0]]))
+		for pair in self.trainIterator:
+			for i in pair[1]:
+				for j in pair[0] + ["NULL"]:
+					self.ewords[j].add(i)
+		return parameters
+
+	def sigma(self, i, j, eSentence, fSentence):
+		num = self.parameters[(eSentence[j], fSentence[i])] * self.q[(j, i, len(eSentence), len(fSentence))]
+		denom = 0.0
+		for l in range(len(eSentence)):
+			denom += (self.parameters[(eSentence[l], fSentence[i])] * self.q[(l, i, len(eSentence), len(fSentence))])
+		return num/denom
+
+	def paramEstimator(self):
+		print "Estimating parameters..."
+		for c in range(self.iterations):
+			print "Iteration " + str(c)
+			counts = defaultdict(int)
+			qCounts = defaultdict(int)
+			tCounts = defaultdict(int)
+			for pair in self.trainIterator:
+				eSentence = ["NULL"] + pair[0]
+				fSentence = pair[1]
+				for i in range(len(fSentence)):
+					for j in range(len(eSentence)):	
+						s = self.sigma(i,j,eSentence,fSentence)
+						counts[(eSentence[j], fSentence[i])] += s
+						counts[(eSentence[j], "TOTAL_COUNTS")] += s
+						qCounts[(j,i,len(eSentence), len(fSentence))] += s
+						tCounts[(i,len(eSentence), len(fSentence))] += s
+			for pair in counts.keys():
+				if pair[1] != "TOTAL_COUNTS":
+					self.parameters[pair] = counts[pair]/(counts[(pair[0], "TOTAL_COUNTS")]*1.0)
+			for q in qCounts.keys():
+				self.q[q] = qCounts[q]/(tCounts[q[1:]]*1.0)
+
+		print "Done estimating parameters."
+		if self.export:
+			self.exportParameters()
+
+	def exportParameters(self):
+		print "Writing parameters to file..."
+		with open("parameters2.txt", 'w') as z:
+			for param in self.parameters.items():
+				z.write(json.dumps(param) + '\n')
+			z.write("*** BEGIN Q ***\n")
+			for q in self.q.items():
+				z.write(json.dumps(q) + '\n')
+		print "Done writing parameters to file."
+
+	def translate(self):
+		print "Beginning translation..."
+		count = 1
+		for pair in self.transIterator:
+			eSentence = ["NULL"] + pair[0]
+			fSentence = pair[1]
+			for i in range(len(fSentence)):
+				maxA = (0, None)
+				for j in range(len(eSentence)):
+					if (self.parameters[(eSentence[j], fSentence[i])] * self.q[(j, i, len(eSentence), len(fSentence))] ) > maxA[0]:
+						maxA = ((self.parameters[(eSentence[j], fSentence[i])] * self.q[(j, i, len(eSentence), len(fSentence))]),j)
+				if maxA[1] != None and maxA[1] != 0:
+					self.alignments.append([count, maxA[1], i+1])
+			count += 1
+		return self.alignments
 
 
+###############################################################################
+###############################################################################
+###############################################################################
 
 if __name__ == '__main__':
-	#trans = Translator('corpus.en', 'corpus.es', 'test.en', 'test.es', 'alignment test.p1.out', 'parameters.txt')
-	trans = Translator('corpus.en', 'corpus.es', 'dev.en', 'dev.es', 'dev.out')#, 'parameters2.txt')
+	#trans = Translator('corpus.en', 'corpus.es', 'test.en', 'test.es', 'alignment_test.p1.out', 1)
+	trans = Translator('corpus.en', 'corpus.es', 'test.en', 'test.es', 'alignment_test.p2.out', 2, 'parameters.txt')
 	trans.countParameters()
 	trans.translate()
 	trans.writeAlignment()
